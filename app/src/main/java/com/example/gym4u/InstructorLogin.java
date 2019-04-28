6d20282ad65a5d7daccdb8a51617af3cff62de00
joezanini <jzanini@ucsc.edu>
2019-04-28 15:53:44 -0700

    changed time of splash screen display. created Gym4u iclauncher icon image.

--- app/src/main/java/com/example/gym4u/InstructorLogin.java
+++ app/src/main/java/com/example/gym4u/InstructorLogin.java
@@ -5,17 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class GettingStartedActivity extends AppCompatActivity {
+public class InstructorLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_getting_started);
+        setContentView(R.layout.activity_instructor_login);
     }
 
-
-    public void instrLogin(View view) {
-        Intent intent = new Intent(this, InstructorLogin.class);
+    public void instrReg(View view) {
+        Intent intent = new Intent(this, InstructorRegister.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/gym4u/InstructorRegister.java
@@ -0,0 +1,13 @@
+package com.example.gym4u;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstructorRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intructor_register);
+    }
+}
