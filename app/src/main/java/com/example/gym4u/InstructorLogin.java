296ee7a16ca2a7aa7d6dc33564cef4f6c4dfe9ee
joezanini <jzanini@ucsc.edu>
2019-04-29 15:16:06 -0700

    added Login and Register activities. Also made some minor changes to java code for those activities to gather info for db.

--- app/src/main/java/com/example/gym4u/InstructorLogin.java
+++ app/src/main/java/com/example/gym4u/InstructorLogin.java
@@ -4,17 +4,41 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class InstructorLogin extends AppCompatActivity {
+public class InstructorLogin extends AppCompatActivity implements View.OnClickListener {
+
+    Button iLogin;
+    EditText iUsername, iPassword;
+    TextView iRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor_login);
+
+        iUsername = (EditText) findViewById(R.id.etUsername);
+        iPassword = (EditText) findViewById(R.id.etPassword);
+        iLogin = (Button) findViewById(R.id.instrLoginBtn);
+        iRegister = (TextView) findViewById(R.id.instrRegisterLink);
+        iLogin.setOnClickListener(this);
+
     }
 
-    public void instrReg(View view) {
-        Intent intent = new Intent(this, InstructorRegister.class);
-        startActivity(intent);
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.instrLoginBtn:
+
+                break;
+
+            case R.id.instrRegisterLink:
+                Intent intent = new Intent(this, InstructorRegister.class);
+                startActivity(intent);
+                break;
+        }
     }
+
 }
--- app/src/main/java/com/example/gym4u/InstructorRegister.java
+++ app/src/main/java/com/example/gym4u/InstructorRegister.java
@@ -2,12 +2,34 @@ package com.example.gym4u;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class InstructorRegister extends AppCompatActivity {
+public class InstructorRegister extends AppCompatActivity implements View.OnClickListener{
+
+    Button iRegister;
+    EditText name, gym, username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intructor_register);
+        iRegister = (Button) findViewById(R.id.instrRegisterBtn);
+        name = (EditText) findViewById(R.id.eName);
+        gym = (EditText) findViewById(R.id.etGym);
+        username = (EditText) findViewById(R.id.etUsername);
+        password = (EditText) findViewById(R.id.etPassword);
+
+        iRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.instrRegisterBtn:
+
+                break;
+        }
     }
 }
