50925ae0b6426de8aa08b3c3496a189ef8bc2fef
joezanini <jzanini@ucsc.edu>
2019-05-01 10:55:23 -0700

    more work on login for clients and DB.

--- app/src/main/java/com/example/gym4u/ClientLocalStore.java
+++ app/src/main/java/com/example/gym4u/ClientLocalStore.java
@@ -16,6 +16,32 @@ public class ClientLocalStore {
         SharedPreferences.Editor spEditor = clientLocalDatabase.edit();
         spEditor.putString("name", client.name);
         spEditor.putString("username", client.username);
+        spEditor.putString("password", client.password);
+        spEditor.putString("gym", client.gym);
+        spEditor.commit();
     }
+
+    public Client getLoggedClient() {
+        String name = clientLocalDatabase.getString("name", "");
+        String username = clientLocalDatabase.getString("username", "");
+        String gym = clientLocalDatabase.getString("gym", "");
+        String password = clientLocalDatabase.getString("password", "");
+
+        Client storedClient = new Client(name, username, gym, password);
+        return storedClient;
+    }
+
+    public void setClientLoggedIn(boolean loggedIn) {
+        SharedPreferences.Editor spEditor = clientLocalDatabase.edit();
+        spEditor.putBoolean("loggedIn", loggedIn);
+        spEditor.commit();
+    }
+
+    public void clearClientData() {
+        SharedPreferences.Editor spEditor = clientLocalDatabase.edit();
+        spEditor.clear();
+        spEditor.commit();
+    }
+
 
 }
