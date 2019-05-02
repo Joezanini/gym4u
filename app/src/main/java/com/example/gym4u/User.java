e2b0ffdfde034be12347e09065372b1698c8ecbf
joezanini <jzanini@ucsc.edu>
2019-05-02 14:29:57 -0700

    chnged setup from there being 2 user objects "CLIENT" & "INSTRUCTOR" to just a user with a client and instructor bool attribute.

--- app/src/main/java/com/example/gym4u/User.java
+++ app/src/main/java/com/example/gym4u/User.java
@@ -2,12 +2,15 @@ package com.example.gym4u;
 
 public class User {
     String name, gym, password, username;
+    boolean instructor, client;
 
-    public User (String name, String username, String gym, String password) {
+    public User (String name, String username, String gym, String password, boolean instructor, boolean client) {
         this.name = name;
         this.username = username;
         this.gym = gym;
         this.password = password;
+        this.instructor = instructor;
+        this.client = client;
     }
 
     public User(String username, String password, String gym) {
